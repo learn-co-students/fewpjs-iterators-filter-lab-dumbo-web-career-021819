286f201808cc9c48f6d5a771b2e41f7139fd5d9d
James Dorr <jamesdorr3@gmail.com>
2019-04-07 02:28:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = (array, name) => array.filter(item => item.toLowerCase() === name.toLowerCase())
+
+// const fuzzyMatch = (array, start) => array.filter(item => item.toLowerCase().slice(0,(start.length)) === start.toLowerCase())
+const fuzzyMatch = (array, start) => {
+    return array.filter(item => {
+        return item.toLowerCase().slice(0,(start.length)) === start.toLowerCase();
+    })
+}
+
+function matchName(objsArray, name) {
+    return objsArray.filter(item => {
+        return item.name === name
+    })
+}
