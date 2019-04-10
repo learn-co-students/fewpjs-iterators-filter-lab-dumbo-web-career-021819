42fbb75ed97e7ff269c73df48f7dd8a9d08689fe
1-8192 <allegranzia@gmail.com>
2019-04-10 12:07:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (array, string) => {
+  return array.filter((item) => {
+    return item.toLowerCase() === string.toLowerCase();
+  });
+};
+
+const fuzzyMatch = (array, string) => {
+  return array.filter((item) => {
+    return item.startsWith(string);
+  })
+};
+
+const matchName = (array, string) => {
+  return array.filter((item) => {
+    return item.name === string;
+  });
+};
